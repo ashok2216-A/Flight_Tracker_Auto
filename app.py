e4e9974d780fcbc416eac737c5351de7310681d1
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2023-09-15 10:57:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,8 +140,8 @@ count = st_autorefresh(interval=10000, limit=1000, key="fizzbuzzcounter")
 if count == 0:
     st.write("Auto Updating Map every 10 Seconds")
 else:
-    time_sec = count + 9
+    count = count + 9
     st.write("Auto Updating Map every 10 Seconds")
-    st.write(f"After: :red[{time_sec}] Seconds")
+    st.write(f"After: :red[{count}] Seconds")
 flight_tracking(flight_view_level=view, country=cou,
             local_time_zone=time, airport=air_port, flight_info=info, color=clr)
